0a22d5f00e0f7bd85a7d6185abde54acb9b1691f
Gean <gsilva@ebsco.com>
2017-05-26 15:59:52 -0300

    validation working

--- src/main/java/com/actminds/customsp/controller/SamlController.java
+++ src/main/java/com/actminds/customsp/controller/SamlController.java
@@ -2,16 +2,12 @@ package com.actminds.customsp.controller;
 
 import com.actminds.customsp.service.SAMLParser;
 import org.opensaml.saml2.core.Response;
-import org.opensaml.xml.io.UnmarshallingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.xml.sax.SAXException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 
 @RestController
 public class SamlController {
--- src/main/java/com/actminds/customsp/service/SAMLParser.java
+++ src/main/java/com/actminds/customsp/service/SAMLParser.java
@@ -1,21 +1,20 @@
 package com.actminds.customsp.service;
 
+import org.apache.xml.security.keys.keyresolver.InvalidKeyResolverException;
 import org.opensaml.Configuration;
 import org.opensaml.DefaultBootstrap;
+import org.opensaml.common.SignableSAMLObject;
 import org.opensaml.common.xml.SAMLConstants;
 import org.opensaml.common.xml.SAMLSchemaBuilder;
+import org.opensaml.saml2.core.Assertion;
 import org.opensaml.saml2.core.Response;
-import org.opensaml.saml2.metadata.EntitiesDescriptor;
-import org.opensaml.saml2.metadata.EntityDescriptor;
 import org.opensaml.saml2.metadata.IDPSSODescriptor;
-import org.opensaml.saml2.metadata.impl.EntitiesDescriptorImpl;
-import org.opensaml.saml2.metadata.provider.HTTPMetadataProvider;
-import org.opensaml.saml2.metadata.provider.MetadataProvider;
 import org.opensaml.saml2.metadata.provider.MetadataProviderException;
 import org.opensaml.security.MetadataCredentialResolver;
 import org.opensaml.security.MetadataCriteria;
 import org.opensaml.security.SAMLSignatureProfileValidator;
 import org.opensaml.xml.ConfigurationException;
+import org.opensaml.xml.XMLObject;
 import org.opensaml.xml.io.Unmarshaller;
 import org.opensaml.xml.io.UnmarshallerFactory;
 import org.opensaml.xml.io.UnmarshallingException;
@@ -23,70 +22,140 @@ import org.opensaml.xml.parse.BasicParserPool;
 import org.opensaml.xml.parse.XMLParserException;
 import org.opensaml.xml.security.CriteriaSet;
 import org.opensaml.xml.security.SecurityException;
+import org.opensaml.xml.security.SecurityHelper;
+import org.opensaml.xml.security.credential.CollectionCredentialResolver;
 import org.opensaml.xml.security.credential.Credential;
 import org.opensaml.xml.security.credential.UsageType;
 import org.opensaml.xml.security.criteria.EntityIDCriteria;
-import org.opensaml.xml.security.criteria.KeyAlgorithmCriteria;
-import org.opensaml.xml.security.criteria.PublicKeyCriteria;
 import org.opensaml.xml.security.criteria.UsageCriteria;
+import org.opensaml.xml.security.keyinfo.CollectionKeyInfoCredentialResolver;
 import org.opensaml.xml.security.keyinfo.KeyInfoCredentialResolver;
-import org.opensaml.xml.security.x509.X509DigestCriteria;
+import org.opensaml.xml.security.x509.X509Util;
 import org.opensaml.xml.signature.Signature;
 import org.opensaml.xml.signature.SignatureTrustEngine;
 import org.opensaml.xml.signature.SignatureValidator;
 import org.opensaml.xml.signature.impl.ExplicitKeySignatureTrustEngine;
+import org.opensaml.xml.util.Base64;
 import org.opensaml.xml.validation.ValidationException;
-import org.opensaml.xml.validation.Validator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.validation.Schema;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
-import java.security.PublicKey;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class SAMLParser {
+    private String cert = "MIICOjCCAaOgAwIBAgIBADANBgkqhkiG9w0BAQ0FADA6MQswCQYDVQQGEwJ1czENMAsGA1UECAwEZ2VhbjENMAsGA1UECgwEZ2VhbjENMAsGA1UEAwwEZ2VhbjAeFw0xNzA1MjYxNzU5MDJaFw0xODA1MjYxNzU5MDJaMDoxCzAJBgNVBAYTAnVzMQ0wCwYDVQQIDARnZWFuMQ0wCwYDVQQKDARnZWFuMQ0wCwYDVQQDDARnZWFuMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdDEwk43Dcko56yhKdr5zABUEw5Uxnb5pNT0EPWkc1F2+T5y7N7kOWmNgurgypOILNw9VRvgDVqgdiWgL8Onj08LDwgeH3o0kfUOEsxtUqv/lqkenJjT4xJL3RklCQfz/AWRorCO7hJg8Dy1syDbiBofRvjcMOWf7YiFn1tTPXIQIDAQABo1AwTjAdBgNVHQ4EFgQUlyJuHZllJCu+272enf8paj7xavcwHwYDVR0jBBgwFoAUlyJuHZllJCu+272enf8paj7xavcwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQ0FAAOBgQBw0Euc+bQlYmWDm5++48a8eZ6T2w6C/6BY7XQc/r0C0VbQRPvm1wlpnq+pmmbKI2KSq9tuqeRaSyTdJLPc5tiwB4yyLRonTT8tWYHpl6l66lFH YVmp9xJN41c+pc4codmYy4U4AkqTUAPG1eWTLTc58llMSMbSC1VxyYpoNl7q7w==";
+    private String privateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJ0MTCTjcNySjnrKEp2vnMAFQTDlTGdvmk1PQQ9aRzUXb5PnLs3uQ5aY2C6uDKk4gs3D1VG+ANWqB2JaAvw6ePTwsPCB4fejSR9Q4SzG1Sq/+WqR6cmNPjEkvdGSUJB/P8BZGisI7uEmDwPLWzINuIGh9G+Nww5Z/tiIWfW1M9chAgMBAAECgYAog8KlBZPZI2nkXXsd+O78Tp65yX3DdXQeG6MSHd0e4jPjuRFHCP+guz+SE06q3SxJfwrqMpUuidWr1sLMZR1M08Z25vJ/Ly/KiYL/X0q5ERHTaMaVJtv+Hx+ItFvkbmgSC/1pF2xXAymvffn1oMmahChVnXgjiR7hpJTI0eOMrQJBAM6qG/F7U4Qki82hb6O7d+aZ8fh2NcVdMOyH04xsd0rA8a117zaLMekSJrJs97XjZ6tULCvyVcmWbfhzN3ao20cCQQDCifojsWS6c4OMfhBKa/z0nUmHsMArX1+YWbqsrrZPdH0tWmq30wt5PbC46RXyRhlYoAY+OJPXdhufJHwYaR5XAkEAh471fVST153RRjSach/J6i0Ylw2S/769FKmTjgynwxUEce9l1bVAK82ILIllgp2DptIzlzACLZTK1aldvCvOiQJARPGVPrUNl658fnvm1hkzSpW7i2UulbB1No8GQ1Cft8T23+3dSEx5Eny0drPUXUpOjUQZMk/mxUNMQrgxao1GfQJAQxl8kOfTPu7Te5HgRZrTgWKXzmwU0HK3yOFl/pXMogN5P8mIRhVrko3qsq3di9dYj7S+6M21187RnXaV5ei05A==";
 
     public SAMLParser() throws ConfigurationException {
         // Initialize the library
         DefaultBootstrap.bootstrap();
     }
 
-    public Response parse(String samlResponse) throws XMLParserException, SAXException, UnmarshallingException, MetadataProviderException, ValidationException, SecurityException {
-        // Initialize DBMetadata Provider
-        DBMetadataProvider dbMetadataProvider = new DBMetadataProvider("http://idp.example.com/metadata.php");
-        dbMetadataProvider.setRequireValidMetadata(true);
-        dbMetadataProvider.setParserPool(new BasicParserPool());
-        dbMetadataProvider.initialize();
+    public Response parse(String samlResponse) throws Exception {
 
-        // Unmarshall SAML Response Posted
-        Response response = unmarshall(samlResponse);
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilderFactory.setNamespaceAware(true);
+        DocumentBuilder docBuilder = documentBuilderFactory.newDocumentBuilder();
 
-        // Validate Signature is in a proper format
-        SAMLSignatureProfileValidator profileValidator = new SAMLSignatureProfileValidator();
-        profileValidator.validate(response.getSignature());
+        Document document = docBuilder.parse(new InputSource(new StringReader(samlResponse)));
+        Element element = document.getDocumentElement();
 
-        // Validate Signature is trusted
-        MetadataCredentialResolver metadataCredentialResolver = new MetadataCredentialResolver(dbMetadataProvider);
-        KeyInfoCredentialResolver keyInfoCredentialResolver = Configuration.getGlobalSecurityConfiguration().getDefaultKeyInfoCredentialResolver();
-        ExplicitKeySignatureTrustEngine explicitKeySignatureTrustEngine = new ExplicitKeySignatureTrustEngine(metadataCredentialResolver, keyInfoCredentialResolver);
-        SignatureTrustEngine signatureTrustEngine = explicitKeySignatureTrustEngine;
-        CriteriaSet criteriaSet = new CriteriaSet();
-        criteriaSet.add(new EntityIDCriteria(response.getIssuer().getValue()));
-        criteriaSet.add(new MetadataCriteria(IDPSSODescriptor.DEFAULT_ELEMENT_NAME, SAMLConstants.SAML20P_NS));
-        criteriaSet.add(new UsageCriteria(UsageType.SIGNING));
-        //criteriaSet.add(new KeyAlgorithmCriteria())
-        Boolean isValid = signatureTrustEngine.validate(response.getSignature(), criteriaSet);
+        UnmarshallerFactory unmarshallerFactory = Configuration.getUnmarshallerFactory();
+        Unmarshaller unmarshaller = unmarshallerFactory.getUnmarshaller(element);
+        XMLObject responseXmlObj = unmarshaller.unmarshall(element);
 
-        if (isValid)
+        Response response = (Response) responseXmlObj;
+
+        Assertion assertion = response.getAssertions().get(0);
+        String subject = assertion.getSubject().getNameID().getValue();
+        String issuer = assertion.getIssuer().getValue();
+        String audience = assertion.getConditions().getAudienceRestrictions().get(0).getAudiences().get(0).getAudienceURI();
+        String statusCode = response.getStatus().getStatusCode().getValue();
+        Signature sig = response.getSignature();
+        X509Certificate certificate = getCertificate(cert);
+        SignatureValidator validator = new SignatureValidator(SecurityHelper.getSimpleCredential(certificate,null));
+        validator.validate(sig);
+
+        if(isValid(response))
             return response;
 
-        throw new RuntimeException("invalid response");
+        throw new RuntimeException();
+    }
+
+    public boolean isValid(SignableSAMLObject object) {
+        try {
+            List<Credential> trustedCredentials = new ArrayList<>();
+            X509Certificate certificate = getCertificate(cert);
+            trustedCredentials.add(SecurityHelper.getSimpleCredential(certificate,null));
+            verifyObjectIsSigned(object);
+            return validateSignature(object, trustedCredentials);
+
+        } catch (SecurityException e) {
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void verifyObjectIsSigned(SignableSAMLObject object) throws SecurityException {
+        if (object.getSignature() == null) {
+            throw new SecurityException("SAML object is not signed");
+        }
+    }
+
+
+    private static java.security.cert.X509Certificate getCertificate(String certificate) throws Exception {
+        try {
+            Collection<java.security.cert.X509Certificate> certificates =
+                    X509Util.decodeCertificate(Base64.decode(certificate));
+            return certificates
+                    .stream()
+                    .findFirst()
+                    .orElseThrow(() -> new Exception("Cannot load certificate"));
+        } catch (CertificateException ex) {
+            throw new Exception("Cannot load certificate", ex);
+        }
+    }
+
+    private boolean validateSignature(SignableSAMLObject object, List<Credential> trustedCredentials) throws SecurityException {
+        KeyInfoCredentialResolver keyInfoResolver = new CollectionKeyInfoCredentialResolver(trustedCredentials);
+        CollectionCredentialResolver credentialResolver = new CollectionCredentialResolver(trustedCredentials);
+        SignatureTrustEngine trustEngine = new ExplicitKeySignatureTrustEngine(credentialResolver, keyInfoResolver);
+        CriteriaSet criteriaSet = buildCriteriaSet();
+        return trustEngine.validate(object.getSignature(), criteriaSet);
+    }
+
+    /**
+     * Builds the criteria set used to validate the signature.
+     *
+     * @return the new criteria set.
+     */
+    private CriteriaSet buildCriteriaSet() {
+        CriteriaSet criteriaSet = new CriteriaSet();
+        criteriaSet.add(new UsageCriteria(UsageType.SIGNING));
+        return criteriaSet;
     }
 
     @SuppressWarnings("unchecked")
